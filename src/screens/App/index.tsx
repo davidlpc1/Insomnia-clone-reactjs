461022242b8230c4e657b7e773c3902793d9fa80
Davi Lucas <davilucasdlcreator@gmail.com>
2020-12-30 23:02:22 -0300

    MethodsTypesContainer finished in Html and Css

--- src/screens/App/index.tsx
+++ src/screens/App/index.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import {
   AppGrid,
   MethodsTypesContainer,
   BodyContainer,
-  ResultContainer
+  ResultContainer,
+  InsomniaConfig,
+  SearchAndCreateMethodContainer,
+  SearchMethod,
+  ButtonCreateRequest,
+  IconCreateRequest
 } from './styles'
 
+const MethodsTypes = () => {
+  const showPopupToCreateNewRequest = useCallback(() => {
+
+  },[])
+  return (
+    <MethodsTypesContainer>
+      <InsomniaConfig><h2>Insomnia</h2></InsomniaConfig>
+      <SearchAndCreateMethodContainer>
+        <SearchMethod type="text" placeholder="Filter" />
+        <ButtonCreateRequest onClick={showPopupToCreateNewRequest}>
+          <IconCreateRequest className="fas fa-plus-circle" />
+        </ButtonCreateRequest>
+      </SearchAndCreateMethodContainer>
+    </MethodsTypesContainer>
+  )
+}
+ 
 export default function App(){
   return (
     <AppGrid>
-      <MethodsTypesContainer></MethodsTypesContainer>
+      <MethodsTypes />
       <BodyContainer></BodyContainer>
       <ResultContainer></ResultContainer>
     </AppGrid>
--- src/screens/App/styles.ts
+++ src/screens/App/styles.ts
@@ -6,16 +6,53 @@ export const AppGrid = styled('div')`
 `;
 
 export const MethodsTypesContainer = styled('div')`
-  background-color:red;
+  background-color:var(--primary-color);
   height:100vh;
 `;
 
 export const BodyContainer = styled('div')`
-  background-color:blue;
+  background-color:var(--primary-color);
   height:100vh;
 `;
 
 export const ResultContainer = styled('div')`
-  background-color:green;
+  background-color:var(--primary-color);
   height:100vh;
+`;
+
+export const InsomniaConfig = styled('div')`
+    background-color:var(--insomnia-color);
+    width:100%;
+    height:60px;
+    color:var(--white-color);
+    padding-left:10px;
+    padding-top:20px;
+`;
+
+export const SearchAndCreateMethodContainer = styled('div')`
+    display:flex;
+    flex:1;
+    padding:20px;
+    min-height:60px;
+    align-items: center;
+    gap:10px;
+`;
+
+export const SearchMethod = styled('input').attrs({
+    placeholderTextColor: "#3e3f3a"
+  })`
+    background-color:var(--primary-color);
+    color:var(--white-color);
+    border: 1px solid #3e3f3a;
+    border-radius:4px;
+    padding:10px;
+`;
+
+export const ButtonCreateRequest = styled('button')`
+    background-color:transparent;
+`;
+
+export const IconCreateRequest = styled('i')`
+    color:#fff;
+    pointer-events:none;
 `;
